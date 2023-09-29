b4f386dad02f93470c29bb23870c759336461cce
Albert <s11641826@students.duvalschools.org>
2023-09-29 14:30:16 -0400

    Update numberGuess.cs

--- gamingExercises/00_numberGuess/numberGuess.cs
+++ gamingExercises/00_numberGuess/numberGuess.cs
@@ -39,6 +39,7 @@ namespace numberGuess
             int numGuesses = 0; //Number of guesses the player is ALLOWED.
             int numAttempts = 0; //Number of guesses TAKEN.
             int playerScore = 0;
+            int playerGuess = 0;
             int cpuScore = 0;
             string difficulty = "";
             int rangeMin = -1;
@@ -57,12 +58,19 @@ namespace numberGuess
                 rangeMax = 10;
                 numGuesses = 4;
 
-            } else if (NORMAL MODE) {
-                // Code to run.
+            } else if (difficulty == "Normal") {
+                rangeMin = 0;
+                rangeMax = 25;
+                numGuesses = 4;
             } else if (HARD MODE) {
-                // Code to run
+                rangeMin = 0;
+                rangeMax = 50;
+                numGuesses = 3;
             } else {
-                // Code to run if no difficulty is selected.
+                Console.WriteLine("No difficult selected, defaulting to normal")
+                rangeMin = 0;
+                rangeMax = 25;
+                numGuesses = 4;
 
             }
             Console.WriteLine("Minimun: " + rangeMin);
@@ -72,14 +80,40 @@ namespace numberGuess
             // START THE MATCH!
             while (playerScore != 3 && cpuScore != 3) {
                 //Any code you want to run BEFORE each round goes here.
-
-
+                // GENERATE SECRET NUMBER
+                Random rndNum = new Random ();
+                secretNumber = rndNum.next(rangeMin, rangeMax);
+                Console.WriteLine("Player Score:" + playerScore + "\n");
+                Console.WriteLine("CPU Score: " + cpuScore + "\n");
                 // START EACH ROUND
                 for (int i = 0; i < numGuesses ; i++) {
                     // Code to guess number goes here.
-                    
+                    Console.WriteLine("You have used "+ numAttempts + "this round. \n");
+                    Console.WriteLine("You might guess between " + rangeMin + "and " + rangeMax + ".\n");
+                    playerGuess = System.Convert.ToInt32(Console.ReadLine)();
+                    if (playerGuess == secretNumber) {
+                        // Print a success message!
+                        playerScore++;
+                        break;
+                    } else {
+                        if(playerGuess  > secretNumber) {
+                            Console.WriteLine("Your guess is too high!\n");
+                        } else {
+                            Console.WriteLine("Your guess is too low!\n");
+                        }
+                    }
+                    numAttempts++;
                 }
+                if (playerGuess != secretNumber) {
+                    cpuScore++;
+                    // Print a round lost message to the console.
+                } 
 
+            }
+            if (playerScore >= 3) {
+                Console.WriteLine("You have won the game!\n")
+            } else {
+                Console.WriteLine("You have lost the game!\n");
             }
         }
     }
