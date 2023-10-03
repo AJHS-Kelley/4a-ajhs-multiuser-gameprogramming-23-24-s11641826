8ae7b762af5575d3484b9031f06c568fc1f8213e
Albert <s11641826@students.duvalschools.org>
2023-10-03 13:55:57 -0400

    v0.0
    Adding program info, removed Console.WriteLine()

---
+++ computer_science_exercises/02_Collections/Program.cs
@@ -0,0 +1,12 @@
+//02_Collections, Albert Laguerre, v0.0
+using System;
+namespace _02_Collections
+{
+    class Program
+    {
+        static Void Main(string[] urgs)
+        {
+           
+        }
+    }
+}
