9d783431b895df5df4e917ded0cd23d1c2d4fc52
Albert <s11641826@students.duvalschools.org>
2023-11-14 14:43:08 -0500

    programTemplate.cs

---
+++ 5a_exampleGameMethods/programTemplate.cs
@@ -0,0 +1,33 @@
+// Albert  Laguerre, Program Template, v0.01
+using System
+
+namespace UPDATEFOREACHPROGRAM
+{
+    class UPDATEFOREACHPROGRAM
+
+    {
+        int shakeDice = 3; 
+        int bettingAmount = 
+        int 
+        
+        static void shakeDice()
+        {
+            
+        }  
+
+        static void rollDice()
+        {
+
+        }
+
+        static void bettingAmount()
+        {
+
+        }
+
+        static void ()
+        {
+
+        }
+    }
+}
