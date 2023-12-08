0371d954040f1252f8565e8c6a180583e2631793
Albert <s11641826@students.duvalschools.org>
2023-12-08 12:05:08 -0500

    programTemplate.cs

--- 5a_exampleGameMethods/programTemplate.cs
+++ 5a_exampleGameMethods/programTemplate.cs
@@ -6,86 +6,88 @@ namespace UPDATEFOREACHPROGRAM
     class UPDATEFOREACHPROGRAM
 
     {
-        int shakeDice = 3; 
-        int bettingAmount = ''
-        int numberofDice = 2
+    static void Main(string[] args)
         
-        static void shakeDice()
-        {
-           
-        }  
+        int = player1 = 0;
+        int = player1wins = 0;
+        int = player2 = 0;
+        int = player2wins = 0;
+        int = rollDice = 0;
+        int = rounds = 1;
 
         static void rollDice()
         {
-                rollDice(numDice, sizeDice):
-                numRolled = 0
-                sum = 0
-                while numRolled < numDice:
+            rollDice(numDice, sizeDice):
+            numRolled = 0
+            sum = 0
+            while numRolled < numDice:
                 roll = random.randint(1, sizeDice)
                 sum += roll
                 print(f"Roll: {roll}\n")
                 print(f"Sum: {sum}\n")
                 numRolled += 1
-                
-                diceRoll = random.randint(1, 6)
-                return diceRoll
-        }
+        }   
 
-        static void bettingAmount()
+        static void diceRoll()
         {
-            
-        }
-
-        static void numberofDice()
-        {
-            numberofDice = 2
-            dice = int(input("Dice: "))
-
-        if (dice <= 0):
-            print("Must at least have one dice!")
-
-        sides = int(input("Sides: "))
-
-        if (sides <= 0):
-            print("Must at least have one side!")
-
-        roll = []
-
-        for i in range(0,dice):
-            face = random.randint(1,sides)
-            roll.append(face)
-
-        print(roll)
-
-        random.randint(1,6)
-
-        }
+            diceRoll = random.randint(1, 6)
+            return diceRoll
+        }   
 
         static void rounds()
         {
             print("Round " + str(rounds))
-        player1 = rollDice()
-        player2 = rollDice()
-        print("Player 1 Roll: " + str(player1))
-        print("Player 2 Roll: " + str(player2))
+            player1 = rollDice()
+            player2 = rollDice()
+            print("Player 1 Roll: " + str(player1))
+            print("Player 2 Roll: " + str(player2))
 
-        if player1 == player2:
-            print("Draw!")
-        elif player1 > player2:
-            player1wins = player1wins + 1
-            print("Player 1 wins!")
-        else:
-            player2wins = player2wins + 1
-            print("Player 2 wins!")
+            if player1 == player2:
+                print("Draw!")
+            elif player1 > player2:
+                player1wins = player1wins + 1
+                print("Player 1 wins!")
+            else:
+                player2wins = player2wins + 1
+                print("Player 2 wins!")
     
             rounds = rounds + 1 
 
-        if player1wins == player2wins:
-            print("Draw!")
-        elif player1wins > player2wins:
-            print("Player 1 Wins! ")
-        else:
-            print("Player 2 Wins! ")
-        }
-    }
+            if player1wins == player2wins:
+                print("Draw!")
+            elif player1wins > player2wins:
+                print("Player 1 Wins! ")
+            else:
+                print("Player 2 Wins! ")
+        }   
+
+        static void ()
+        {
+            dice = int(input("Dice: "))
+
+            if (dice <= 0):
+                print("Must at least have one dice!")
+
+            sides = int(input("Sides: "))
+
+            if (sides <= 0):
+                print("Must at least have one side!")
+
+            roll = []
+
+            for i in range(0,dice):
+                face = random.randint(1,sides)
+                roll.append(face)
+
+            print(roll)
+
+            random.randintshuifshuffle(roll)
+            random.randint(1,6)
+        }   
+
+
+    }    
+
+       
+    
 }
