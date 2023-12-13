d1e05e564369b7bc2f5e33cf8d1157ccdbd68f18
Albert <s11641826@students.duvalschools.org>
2023-12-13 11:48:56 -0500

    programTemplate.cs

--- 5a_exampleGameMethods/programTemplate.cs
+++ 5a_exampleGameMethods/programTemplate.cs
@@ -21,3 +21,24 @@ class Program
             Console.WriteLine($"Player 1 rolled a {dice1}");
             player1Score += dice1;
 
+            Console.WriteLine("Player 2, press Enter to roll the dice...");
+            Console.ReadLine();
+            int dice2 = random.Next(1, 7);
+            Console.WriteLine($"Player 2 rolled a {dice2}");
+            player2Score += dice2;
+        }
+
+        Console.WriteLine("\nGame Over!");
+        Console.WriteLine($"Player 1 Score: {player1Score}");
+        Console.WriteLine($"Player 2 Score: {player2Score}");
+
+        if (player1Score > player2Score)
+            Console.WriteLine("Player 1 wins!");
+        else if (player2Score > player1Score)
+            Console.WriteLine("Player 2 wins!");
+        else
+            Console.WriteLine("It's a tie!");
+
+        Console.ReadLine(); 
+    }
+}
